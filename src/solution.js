3ff870571dce6841e088f9b8fa4306e4f9fc7980
CuriosBasant <basantbrpl@gmail.com>
2021-07-30 21:01:41 +0530

    update package.json

--- src/solution.js
+++
@@ -1,9 +0,0 @@
-function solution(str:string, ending:string) {
-  let doesIt = false
-  for (let i = ending.length-1; i > -1; i--) {
-    let isChSame = false
-    if()
-    
-  }
-  return doesIt
-}
